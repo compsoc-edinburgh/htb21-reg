69feb47f5355a3436e7ab4dbca00dfdf254bd002
tcmal <oscar.shrimpton.personal@gmail.com>
2022-01-30 17:50:00 +0000

    some helper scripts

---
+++ scripts/admit_all.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Admits users from a .csv file with their email addresses in.
+# Emails should be in the first column and can be login or contact emails.
+
+from csv import reader
+from sys import argv
+import requests
+
+SERVER_BASE = "https://register.2022.hacktheburgh.com/"
+API_KEY = getenv("HTB_API_KEY")
+API_SECRET = getenv("HTB_API_SECRET")
+
+session = requests.Session()
+
+session.headers.update({'Authorization': "Bearer %s/%s" % (API_KEY, API_SECRET)})
+
+if len(argv) != 2:
+    print("Usage: ./admit_all.py admitted.csv")
+    exit(1)
+
+filename = argv[1]
+f = open(filename, "r")
+r = reader(f)
+
+def get_user_by_email(email):
+    r = session.get(SERVER_BASE + f"api/v2/applicants/by_email?email={email}").json()
+    if r['ok']:
+        return r['data']
+    else:
+        raise Exception(r['message'])
+
+def admit_user_id(i):
+    r = session.post(SERVER_BASE + "api/v2/applicants/admit", json={'app_id': i, 'admit': True}).json()
+    if r['ok']:
+        return True
+    else:
+        raise Exception(r['message'])
+    pass
+
+for row in r:
+    email = row[0]
+    try:
+        user = get_user_by_email(email)
+        print(f"{email} - {user['user_id']}")
+        assert admit_user_id(user['user_id'])
+    except Exception as e:
+        print("Error admitting %s: %s" % (email, str(e)))
---
+++ scripts/get_cvs.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Get names, emails, and CV URLs from applicants.
+# This data is intended to be given to sponsors, and so it excludes those who haven't agreed to share their info this way.
+# By default, only includes those who have been admitted. Pass --all to override this.
+
+from csv import writer
+from sys import stdout, argv
+from os import getenv
+import requests
+
+SERVER_BASE = "https://register.2022.hacktheburgh.com/"
+CV_BASE_URL = "https://htb8-test.s3.eu-west-2.amazonaws.com/htb21-cvs/"
+API_KEY = getenv("HTB_API_KEY")
+API_SECRET = getenv("HTB_API_SECRET")
+
+only_admitted = True
+if len(argv) > 1 and argv[1] == "--all":
+    only_admitted = False
+
+session = requests.Session()
+session.headers.update({'Authorization': "Bearer %s/%s" % (API_KEY, API_SECRET)})
+
+def get_all_users():
+    r = session.get(SERVER_BASE + "api/v2/applicants/all/json?completed=1").json()
+    if r['ok']:
+        return r['data']
+    else:
+        raise Exception(r['message'])
+    pass
+
+w = writer(stdout)
+w.writerow(['First Name', 'Last Name(s)', 'Email', 'CV', 'Admitted?'])
+for user in get_all_users():
+    if (only_admitted and not user['admitted']) or not user['gdpr_sponsor']:
+        continue
+
+    cv_url = f"{CV_BASE_URL}{user['user_id'].replace(':', '_')}.pdf"
+    w.writerow([user['first_name'], user['last_name'], user['contact_email'], cv_url, user['admitted'] == 1])
